33b18e7c7a9a6105c5f0bb642f549ec6e312aaa3
yasmeeen16 <yasmeen16@gmail.com>
2019-10-13 14:22:07 -0700

    Initial Commit

---
+++ FootballLeaguesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FootballLeaguesApp
+//
+//  Created by yasmeen on 10/13/19.
+//  Copyright © 2019 yasmeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
